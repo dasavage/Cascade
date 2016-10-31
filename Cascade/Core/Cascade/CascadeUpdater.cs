d62545670a1aec80777abc2c6a6e42b1fc34bd34
Liam Savage <liamatworld@gmail.com>
2016-10-31 16:05:21 +0000

    Improoved how it checked for updates.
    Improoved how it checked for updates.

--- Cascade/Core/Cascade/CascadeUpdater.cs
+++ Cascade/Core/Cascade/CascadeUpdater.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Cascade.Core.Logging;
+using System.Timers;
 
 namespace Cascade.Core.Cascade
 {
-    class CascadeUpdater
+    internal class CascadeUpdater
     {
+        // AAAAAAAAA System.Timers.Timer...
+
+        private readonly LogManager _logManager;
+
+        public CascadeUpdater()
+        {
+            _logManager = global::Cascade.Cascade.GetLogManager();
+
+            var configManager = global::Cascade.Cascade.GetConfigManager();
+
+            if (!configManager.ConfigElementExists("cascade.check_for_updates") || configManager.GetConfigElement("cascade.check_for_updates") != "yes")
+            {
+                return;
+            }
+
+            var myTimer = new Timer();
+            myTimer.Elapsed += CheckForUpdate;
+            myTimer.Interval = 10 * 60000;
+
+            if (configManager.ConfigElementExists("cascade.check_for_updates_every_x_minutes"))
+            {
+                myTimer.Interval = int.Parse(configManager.GetConfigElement("casecade.check_for_updates_every_x_minutes")) * 60000;
+            }
+
+            myTimer.Enabled = true;
+        }
+
+
+        private void CheckForUpdate(object source, ElapsedEventArgs e)
+        {
+            var updateString = Utility.UpdateAvalible();
+
+            if (!string.IsNullOrEmpty(updateString) && Utility.ValidVersionString(updateString))
+            {
+                _logManager.Log("Update Avalible: " + updateString + " is now avalible.", LogType.Warning);
+            }
+        }
     }
 }
